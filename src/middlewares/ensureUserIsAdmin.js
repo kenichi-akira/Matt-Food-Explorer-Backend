3234fef62b28ab0dcb77d2f684472f717df587d3
kenichi-akira <161388141+kenichi-akira@users.noreply.github.com>
2024-11-13 10:01:26 -0300

    Add files via upload

---
+++ src/middlewares/ensureUserIsAdmin.js
@@ -0,0 +1,21 @@
+
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+    
+//Verificação de administrador
+async function ensureUserIsAdmin(request, response, next) {
+
+    const user_id = request.user.id;
+
+
+    const user = await knex("users").where({id: user_id}).first();
+
+
+    if (!user.isAdmin) {
+        throw new AppError("Access Denied: Unauthorized User", 401)
+    }
+
+    next();
+}
+
+module.exports = ensureUserIsAdmin;
